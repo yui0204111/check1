5355901055ffd933277d431778e7896e964e848e
yui0204111 <ayuiyui0204@gmail.com>
2024-06-25 14:13:50 +0900

    課題提出
    一章チェックテスト

---
+++ main.js
@@ -0,0 +1,18 @@
+let numbers = [2,5,12,13,15,18,22];
+function isEven(number){
+    return number%2===0;
+}
+let num= numbers.filter(isEven);
+console.log(num+'は偶数です');
+
+class car{
+    constructor(gas,numbe){
+        this.gas=gas
+        this.numbe=numbe
+    }
+    showValue(){
+        console.log(`ガソリンは${this.gas}で、ナンバーは${this.numbe}です`)
+    }
+}
+let getNumGas =new car(200,13);
+getNumGas.showValue();
